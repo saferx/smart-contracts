396f50a46bc1cd7049cebbaf9be0a6de475cc3b0
Shashank Acharya <acharya.s@icloud.com>
2023-04-22 14:53:18 +0800

    Update deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,14 +1,15 @@
 async function main() {
 	const UserRegistry = await ethers.getContractFactory("UserRegistry");
+	console.log('Deploying UserRegistry...')
 	const user_registry = await UserRegistry.deploy();
 	const user_registry_address = user_registry.address
+	console.log("UserRegistry deployed to address:", user_registry_address);
 
 	const Medication = await ethers.getContractFactory("Medication");
+	console.log('Deploying Medication...')
 	const medication = await Medication.deploy(user_registry_address);
 	const medication_address = medication.address
-
-	console.log("UserRegistry deployed to address:", user_registry_address);
-	console.log("Medication deployed to address:"  , medication_address);
+	console.log("Medication deployed to address:", medication_address);
 }
 
 main()
